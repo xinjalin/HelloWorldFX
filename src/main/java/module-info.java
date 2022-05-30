b3376e34eb8389cb042056c0ffae1c13df2eca29
Osborn <2161885@cognizant.com>
2022-05-30 11:36:43 +0930

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.helloworldfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.helloworldfx to javafx.fxml;
+    exports com.example.helloworldfx;
+}
